56e0bd9fa810c9aad8fd37ebf59234c5cea20ac5
JamieHome <j.d.collier@warwick.ac.uk>
2021-01-24 19:31:58 +0000

    Create Button parent class which is inherited by all individual button classes

--- clear_button.py
+++ clear_button.py
@@ -1,34 +1,18 @@
-import pygame.font
+from button import Button
+import pygame
 
-
-class ClearButton:
+class ClearButton(Button):
 
     def __init__(self, screen):
-        """Initialize the buttons attributes"""
-        self.screen = screen
-        self.screen_rect = screen.get_rect()
-
-        # Set the dimensions and properties of the button
         self.width, self.height = 200, 50
-        self.text_color = (250, 250, 250)
-        self.font = pygame.font.SysFont(None, 48) # Default font at size 48
+        self.fontsize = 48
+        super().__init__(screen, self.width, self.height, 48)
 
-        # Build the button's rect object and center it
-        self.rect = pygame.Rect(0, 0, self.width, self.height)
+        # Overwrite default values
+        self.text_color = (250, 250, 250)
         self.button_color = (220, 80, 80)
+
         self.rect.centerx = self.screen_rect.centerx + 110
         self.rect.centery = self.screen_rect.centery + 305
         self.msg = 'Clear'
         self.prep_msg()
-
-    def prep_msg(self):
-        """Turn msg into a rendered image and center text on the button"""
-        self.msg_image = self.font.render(self.msg, True, self.text_color,
-                                          self.button_color)
-        self.msg_image_rect = self.msg_image.get_rect()
-        self.msg_image_rect.center = self.rect.center
-
-    def draw_button(self):
-        """Draw blank button and then draw message on the screen"""
-        self.screen.fill(self.button_color, self.rect)
-        self.screen.blit(self.msg_image, self.msg_image_rect)
